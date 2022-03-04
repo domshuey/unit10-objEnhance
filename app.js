34da10781ec2c20001b2d8f75e4e9ec2815017d1
Dominic Shuey <dominic.shuey@gmail.com>
2022-03-04 10:39:04 -0500

    adding files

---
+++ app.js
@@ -0,0 +1,55 @@
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+function createInstructor(firstName, lastName){
+    return {
+        firstName,
+        lastName
+    }
+}
+
+// var favoriteNumber = 42;
+// var instructor = {firstName: "Colt"};
+// instructor[favoriteNumber] = "That is my favorite!"
+
+const favoriteNumber = 42;
+const instructor = {
+    firstName: 'Colt',
+    [favoriteNumber]: "that is my favorite!"
+    };
+
+    // var instructor = {
+    //     firstName: "Colt",
+    //     sayHi: function(){
+    //       return "Hi!";
+    //     },
+    //     sayBye: function(){
+    //       return this.firstName + " says bye!";
+    //     }
+    //   }
+
+    const instructor1 = {
+        firstName : 'Colt',
+        sayHi() {return 'HI!'},
+        sayBye() {return this.firstName + 'says bye!'}
+    };
+
+
+//    const d = createAnimal("dog", "bark", "Woooof!")
+    // {species: "dog", bark: ƒ}
+    // d.bark()  //"Woooof!"
+
+// const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+    // {species: "sheep", bleet: ƒ}
+    // s.bleet() //"BAAAAaaaa"
+
+function createAnimal(species, verb, noise) {
+    return {
+        species,
+        [verb](){return noise}
+    }
+}
